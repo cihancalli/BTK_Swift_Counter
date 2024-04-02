c8cd109887b06a1acfe980bf9c1983256d84b1d6
Cihan Çallı <cihan.callii@gmail.com>
2024-04-02 10:27:31 +0300

    first commit

--- Counters/ViewController.swift
+++ Counters/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelTime: UILabel!
+    
+    var timer = Timer()
+    var remainingTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        remainingTime = 15
+        labelTime.text = "Time: \(remainingTime)"
     }
 
-
+    @IBAction func buttonStart(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerFunction() {
+        labelTime.text = "Time: \(remainingTime)"
+        remainingTime = remainingTime - 1
+        
+        if remainingTime == 0 {
+            labelTime.text = "Time Out"
+            timer.invalidate()
+            remainingTime = 15
+        }
+    }
+    
 }
 
